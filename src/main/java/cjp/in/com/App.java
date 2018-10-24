465c7a2c3a01eaa176de613ba2cac9e94cf80f7a
cjpcloud <35867500+cjpcloud@users.noreply.github.com>
2018-10-24 08:21:43 +0530

    Update App.java

--- src/main/java/cjp/in/com/App.java
+++ src/main/java/cjp/in/com/App.java
@@ -10,7 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         system.out.println("Hello baboi");      
-        system.out.println("Hello");
+        system.out.println("hi");
 
         
     }
